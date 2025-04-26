e172b69c63e58a296986bde07d5be3d92820210b
Xavier <xavierlesa@gmail.com>
2025-04-26 17:57:56 -0300

    update comments

--- spi.py
+++ spi.py
@@ -5,8 +5,8 @@ import time
 
 SPI_BUS = 0             # spidev0
 SPI_SS  = 0             # spidev0.0
-SPI_CLOCK = 3000000     # 3 Mhz
-CP = 18
+SPI_CLOCK = 1000000     # 1 Mhz
+CP = 18                 # GPIO 18 as CP
 
 # setup SPI
 spi = spidev.SpiDev(SPI_BUS, SPI_SS)
@@ -17,16 +17,22 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(CP, GPIO.OUT)
 
 # register labels
-RCLK = 1 << 7
-RXOE = 1 << 6
-RYOE = 1 << 5
+RCLK = 1 << 7       # bit 7 del segundo byte, es decir bit 15 del word. 
+RXOE = 1 << 6       # bit 6 del segundo byte, es decir bit 14 del word. 
+RYOE = 1 << 5       # bit 5 del segundo byte, es decir bit 13 del word. 
+
+# TODO: para elegir el WR
+RR2 = 1 << 2        # bit 2 del segundo byte, es decir bit 10 del word. 
+RR1 = 1 << 1        # bit 1 del segundo byte, es decir bit 9 del word. 
+RR0 = 1 << 0        # bit 0 del segundo byte, es decir bit 8 del word. 
 
 def latch():
+    """Send a latch pulse (bit 15)"""
     GPIO.output(CP, GPIO.HIGH)
     GPIO.output(CP, GPIO.LOW)
 
 try:
-    b = 128
+    b = 0x80
     while True:
         spi.writebytes([RYOE, b])
         latch()
